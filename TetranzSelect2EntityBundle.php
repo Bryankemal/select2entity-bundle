fa060d22dcf1ce9f2e0e7a776ca39b971e049f80
Bryankemal <35216106+Bryankemal@users.noreply.github.com>
2024-08-07 16:38:58 +0200

    Update TetranzSelect2EntityBundle.php

--- TetranzSelect2EntityBundle.php
+++ TetranzSelect2EntityBundle.php
@@ -2,7 +2,7 @@
 
 namespace Tetranz\Select2EntityBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class TetranzSelect2EntityBundle extends AbstractBundle
 {
